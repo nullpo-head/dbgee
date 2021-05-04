0c3d25f88db01dc81a55951a742acaa3038d4c4d
Takaya Saeki <abc.tkys+pub@gmail.com>
2021-05-04 23:01:06 +0900

    Add a test for set subcommand

--- dbgee/tests/integration_test.rs
+++ dbgee/tests/integration_test.rs
@@ -1,11 +1,11 @@
-use std::{env, path::PathBuf, process::Command, str::FromStr};
+use std::{env, fs, path::PathBuf, process::Command, str::FromStr};
 
 use anyhow::Result;
 
 #[test]
 fn test_run_pid_debugger() -> Result<()> {
     set_fake_commands_path()?;
-    let bin_pathbuf = get_bin_path();
+    let dbgee_pathbuf = get_bin_path();
     let langs = ["c", "rust"];
 
     for lang in langs.iter() {
@@ -22,8 +22,7 @@ fn test_run_pid_debugger() -> Result<()> {
             "arg0".to_owned(),
             "arg1".to_owned(),
         ];
-        let output = Command::new(bin_pathbuf.as_os_str()).args(cmd).output()?;
-        eprintln!("{}", &String::from_utf8(output.stderr)?);
+        let output = Command::new(dbgee_pathbuf.as_os_str()).args(cmd).output()?;
         assert_eq!(Some(0), output.status.code());
         assert_eq!(
             "'new-window' 'gdb' '-p' '<NUM>' \nhello\n",
@@ -37,7 +36,7 @@ fn test_run_pid_debugger() -> Result<()> {
 #[test]
 fn test_run_dlv() -> Result<()> {
     set_fake_commands_path()?;
-    let bin_pathbuf = get_bin_path();
+    let dbgee_pathbuf = get_bin_path();
 
     let cmd = vec![
         "run".to_owned(),
@@ -51,7 +50,7 @@ fn test_run_dlv() -> Result<()> {
         "arg0".to_owned(),
         "arg1".to_owned(),
     ];
-    let output = Command::new(bin_pathbuf.as_os_str()).args(cmd).output()?;
+    let output = Command::new(dbgee_pathbuf.as_os_str()).args(cmd).output()?;
     assert_eq!(Some(0), output.status.code());
     let stdout = String::from_utf8(output.stdout)?
         .lines()
@@ -60,6 +59,45 @@ fn test_run_dlv() -> Result<()> {
     // The command line for Delve is long, so test only part of it here
     assert!(stdout[0].starts_with("'exec' '--headless'"));
     assert!(stdout[1].starts_with("'new-window' 'dlv' 'connect'"));
+
+    Ok(())
+}
+
+#[test]
+fn test_set_pid_debugger() -> Result<()> {
+    set_fake_commands_path()?;
+
+    let copied_hello = CopiedExecutable::new(&format!(
+        "{}/lang_projects/c/hello",
+        get_tests_dir()?.to_str().unwrap()
+    ))?;
+
+    let dbgee_pathbuf = get_bin_path();
+    let cmd_to_set = vec!["set", "-t", "tmuxw", &copied_hello.path];
+    let status = Command::new(dbgee_pathbuf.as_os_str())
+        .args(cmd_to_set)
+        .status()?;
+    assert_eq!(Some(0), status.code());
+
+    let debuggee_output = Command::new(&copied_hello.path).output()?;
+    assert_eq!(Some(0), debuggee_output.status.code());
+    assert_eq!(
+        "'new-window' 'gdb' '-p' '<NUM>' \nhello\n",
+        &String::from_utf8(debuggee_output.stdout)?
+    );
+
+    let cmd_to_unset = vec!["unset", &copied_hello.path];
+    let status = Command::new(dbgee_pathbuf.as_os_str())
+        .args(cmd_to_unset)
+        .status()?;
+    assert_eq!(Some(0), status.code());
+
+    let original_debuggee_output = Command::new(&copied_hello.path).output()?;
+    assert_eq!(Some(0), debuggee_output.status.code());
+    assert_eq!(
+        "hello\n",
+        &String::from_utf8(original_debuggee_output.stdout)?
+    );
 
     Ok(())
 }
@@ -85,13 +123,31 @@ fn get_bin_path() -> PathBuf {
         pathbuf.pop();
     }
     pathbuf.push("dbgee");
-    eprintln!("{}", pathbuf.as_os_str().to_str().unwrap());
     pathbuf
 }
 
 fn get_tests_dir() -> Result<PathBuf> {
     let mut pathbuf = PathBuf::from_str(&env::var("CARGO_MANIFEST_DIR")?)?;
     pathbuf.push("tests");
-    eprintln!("path: {}", pathbuf.to_str().unwrap());
     Ok(pathbuf)
+}
+
+struct CopiedExecutable {
+    path: String,
+}
+
+impl CopiedExecutable {
+    fn new(path: &str) -> Result<CopiedExecutable> {
+        let copied_path = "/tmp/dbgee-copied-debuggee";
+        fs::copy(&path, copied_path)?;
+        Ok(CopiedExecutable {
+            path: copied_path.to_string(),
+        })
+    }
+}
+
+impl Drop for CopiedExecutable {
+    fn drop(&mut self) {
+        let _ = fs::remove_file(&self.path);
+    }
 }
